8fb6a52c87385775682721f38a91f73940ec1bf0
katsumag <44585166+katsumag@users.noreply.github.com>
2024-10-24 19:26:25 +0100

    Remove attribution from building classes

--- core/src/main/java/io/github/eng1g4/building/impl/LectureTheatre.java
+++ core/src/main/java/io/github/eng1g4/building/impl/LectureTheatre.java
@@ -5,8 +5,6 @@ import io.github.eng1g4.building.BuildingType;
 
 public class LectureTheatre extends Building {
 
-    // REQUIRES ATTRIBUTION
-    // https://www.freepik.com/free-vector/isometric-cinema-composition_5973013.htm#fromView=search&page=1&position=15&uuid=425cc2e6-9053-4654-a7ff-f76715058685
     private static final String TEXTURE_PATH = "lecture_theatre.png";
 
     public LectureTheatre(int x, int y) {
--- core/src/main/java/io/github/eng1g4/building/impl/Restaurant.java
+++ core/src/main/java/io/github/eng1g4/building/impl/Restaurant.java
@@ -5,8 +5,6 @@ import io.github.eng1g4.building.BuildingType;
 
 public class Restaurant extends Building {
 
-    // REQUIRES ATTRIBUTION
-    // https://www.freepik.com/free-vector/restaurant-cut-view_1304799.htm#fromView=search&page=1&position=2&uuid=8af6ec5a-f2c1-49d4-ad93-fedb4a77aab5
     private static final String TEXTURE_PATH = "restaurant.png";
 
     public Restaurant(int x, int y) {
